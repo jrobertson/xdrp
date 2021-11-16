134068c544cab0b436500413e2c3588b9dd2c92b
James Robertson <james@jamesrobertson.me.uk>
2021-11-16 12:51:10 +0000

    bug fix: Now works with Ruby 3.0.2. Hash object is now dereferenced before passing into method.

--- lib/xdrp.rb
+++ lib/xdrp.rb
@@ -19,22 +19,22 @@ MOUSE = 1
 KEYBOARD = 2
 KEYBOARD_MOUSE = 3
 
-  
+
 module Xdrp
 
   class Recorder
 
     attr_reader :xml
-    
+
     # level:
     #    1 = mouse
     #    2 = keyboard
     #    3 = keyboard + mouse
 
     def initialize(levelx=KEYBOARD_MOUSE, level: levelx, debug: false)
-      
+
       @mouse, @keyboard = false, false
-      
+
       case level
       when 1
         @mouse = true
@@ -46,13 +46,13 @@ module Xdrp
 
 
       @xiw = XInputWrapper.new(verbose: true, debug: debug, callback: self)
-      
+
       @wm = WMCtrl.display
       @win_title = ''
-      
+
     end
 
-    def on_keypress(key, keycode, modifier=nil)      
+    def on_keypress(key, keycode, modifier=nil)
 
       if @debug then
         puts 'key: ' + key.inspect
@@ -61,32 +61,32 @@ module Xdrp
       end
 
       stop() if modifier and modifier[0] == :alt and key == :z
-      
-      return unless @keyboard      
+
+      return unless @keyboard
 
       add_sleep() if Time.now > (@t1 + 2)
-      
+
 
       if @a.length >= 1 and @a[-1][0] == :type then
 
         if modifier.nil? or (modifier and modifier.empty?) then
           @a[-1][2] += key.to_s.sub('{space}', ' ')
         else
-          
+
           if modifier.length < 2  then
-            
+
             if modifier.first == :shift and (keycode.between?(10,21) \
                                              or keycode.between?(24,35) \
                                              or keycode.between?(38,48)) then
-              
+
               char = if key.to_s =~ /[a-z]/ then
                 key.to_s.upcase
               elsif key.to_s =~ /[0-9]/
                 %w[) ! " £ $ % ^ & * ( ][key.to_s.to_i]
               else
-                                
+
                 lsym = %i(` - = [ ] ; ' # \ , . /)
-                
+
                 if lsym.include? key then
 
                   usym = %w(¬ _ + { } : @ ~ | < > ?)
@@ -95,14 +95,14 @@ module Xdrp
                 end
 
               end
-              
+
               @a[-1][2] += char
-              
+
             else
-              @a << [modifier.first, {key: key}] 
+              @a << [modifier.first, {key: key}]
             end
           end
-        end        
+        end
 
       else
 
@@ -123,9 +123,9 @@ module Xdrp
 
     def on_mousedown(button, x, y)
 
-      return unless @mouse      
-      monitor_app()      
-      
+      return unless @mouse
+      monitor_app()
+
       puts "mouse %s button down at %s, %s" % [button, x, y] if @debug
       add_sleep() if Time.now > (@t1 + 2)
       @a << [:mouse, {click: button, x: x, y: y}]
@@ -133,7 +133,7 @@ module Xdrp
     end
 
     def on_mouseup(button, x, y)
-      
+
       return unless @mouse
       puts "mouse %s button up at %s, %s" % [button, x, y] if @debug
     end
@@ -141,7 +141,7 @@ module Xdrp
     def on_mousemove(x, y)
 
       return unless @mouse
-      
+
       puts 'mouse is moving at ' + x.to_s + ' y ' + y.to_s if @debug
       add_sleep() if Time.now > (@t1 + 2)
       @a << [:mousemove, {x: x, y: y}]
@@ -151,7 +151,7 @@ module Xdrp
     def on_mouse_scrolldown()
 
       return unless @mouse
-      
+
       puts 'mouse is scrolling down' if @debug
       add_sleep() if Time.now > (@t1 + 2)
       @a << [:mousewheel, {scroll: 'down'}]
@@ -161,32 +161,32 @@ module Xdrp
     def on_mouse_scrollup()
 
       return unless @mouse
-      
+
       puts 'mouse is scrolling up ' if @debug
       add_sleep() if Time.now > (@t1 + 2)
       @a << [:mousewheel, {scroll: 'up'}]
 
     end
-    
+
     def save(filepath)
       File.write filepath, xml()
     end
 
     def start()
-      
+
       @a = []
       Thread.new { @xiw.listen }
       puts 'recording ...'
       @t1 = Time.now
-      
+
     end
 
     def stop()
-      
+
       @xiw.stop = true
       @doc = Rexle.new(['xdrp', {}, '', *@a])
       puts 'recording stopped'
-      
+
     end
 
     def xml()
@@ -199,21 +199,22 @@ module Xdrp
       @a << [:sleep, {duration: (Time.now - @t1).round}]
       @t1 = Time.now
     end
-    
-    def monitor_app()      
 
-      win = @wm.windows.find {|x| x.active}    
-      
+    def monitor_app()
+
+      win = @wm.windows.find {|x| x.active}
+
       if win.title != @win_title then
         @win_title = win.title
         @a << [:window, {activate: win.title}]
       end
-      
+
     end
 
   end
 
   class Player
+    using ColouredText
 
     def initialize(src, debug: false)
 
@@ -226,8 +227,17 @@ module Xdrp
     def play()
 
       @doc.root.elements.each do |e|
-        puts 'e: ' + e.xml.inspect if @debug
-        method('xdo_' + e.name.to_s).call(e.text || e.attributes)
+
+        if @debug then
+          puts ('e: ' + e.xml.inspect).debug
+          puts 'e.class: ' + e.class.inspect.debug
+        end
+
+        if e.attributes.any? then
+          method('xdo_' + e.name.to_s).call(**(e.attributes))
+        else
+          method('xdo_' + e.name.to_s).call(e.text)
+        end
       end
 
     end
@@ -239,17 +249,17 @@ module Xdrp
     end
 
     def xdo_mouse(click: 'left', x: 0, y: 0)
-      XDo::Mouse.click(x.to_i, y.to_i, 
+      XDo::Mouse.click(x.to_i, y.to_i,
                        Object.const_get('XDo::Mouse::' + click.upcase))
     end
 
     def xdo_mousemove(x: 0, y: 0)
       XDo::Mouse.move(x.to_i, y.to_i)
     end
-                      
+
     def xdo_mousewheel(scroll: 'down')
       XDo::Mouse.wheel(Object.const_get('XDo::Mouse::' + scroll.upcase, 4))
-    end                      
+    end
 
     def xdo_sleep(duration: 0)
       sleep duration.to_f
@@ -262,15 +272,20 @@ module Xdrp
     def xdo_type(s)
       XDo::Keyboard.simulate(s.gsub('{enter}', "\n"))
     end
-    
+
     def xdo_window(activate: '')
-      
+
+      if @debug then
+        puts 'inside xdo_window'
+        puts 'activate: ' + activate.inspect
+      end
+
       wm = WMCtrl.display
       window = wm.windows.find {|x| x.title == activate}
       window.activate
-      
+
     end
-    
+
 
   end
 
--- xdrp.gemspec
+++ xdrp.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'xdrp'
-  s.version = '0.2.1'
+  s.version = '0.2.2'
   s.summary = 'A basic macro recorder for GNU/Linux which uses program ' + 
       'xinput to capture input events.'
   s.authors = ['James Robertson']
@@ -9,10 +9,11 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency('rxfhelper', '~> 1.1', '>=1.1.3')
   s.add_runtime_dependency('xinput_wrapper', '~> 0.8', '>=0.8.1')    
   s.add_runtime_dependency('ruby-wmctrl', '~> 0.0', '>=0.0.8')
-  s.add_runtime_dependency('keystroker', '~> 0.3', '>=0.3.0')
+  s.add_runtime_dependency('keystroker', '~> 0.3', '>=0.3.1')
   s.signing_key = '../privatekeys/xdrp.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/xdrp'
+  s.required_ruby_version = '>= 3.0.2'
 end
